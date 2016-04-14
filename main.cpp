4ad0c7c6fc4bdb9565e0cc766f36b762d7f82dd8
EdytaEsz <226044@student.pwr.edu.pl>
2016-04-14 19:13:02 +0200

    rozwiązanie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, char **argv)
+void ciag_fibonacciego(int);
+void ciag_arytmetyczny(int);
+
+
+int main(int argc, char** argv)
 {
-	printf("Hello, World!\n");
+    int n = atoi(*(argv+1));
+    char znak = **(argv+2);
+
+    if(znak == 'f')
+    ciag_fibonacciego(n);
+
+    else if(znak == 'a')
+    ciag_arytmetyczny(n);
+
+    else
+    cout << "Podales niewlasciwa litere \n";
+}
+
+void ciag_fibonacciego(int ile_wyrazow)
+{
+    int* t = new int[ile_wyrazow];
+
+    for( int i=0; i<ile_wyrazow; i++ )
+        {
+            if(i<2)
+            t[i] = 1;
+
+            else
+            t[i] = t[i-2] + t[i-1];
+        }
+
+    for ( int i=0; i<ile_wyrazow; i++ )
+    cout << t[i] << endl;
+
+    delete [] t;
+}
+
+
+void ciag_arytmetyczny(int ile_wyrazow)
+{
+    for( int i=1; i<=ile_wyrazow; i++ )
+    {
+        cout << (7*i+2) << endl;  //i-ty wyraz ciagu arytmetycznego
+    }
+
 }
